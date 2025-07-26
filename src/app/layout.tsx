2f044fce04a854c51a064122f247baf480e41229
danblock97 <generalgoldiez@gmail.com>
2025-07-26 09:22:13 +0100

    fix(analytics): added missing google analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -124,6 +124,21 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
+				{process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+					<>
+						<script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}></script>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+									gtag('js', new Date());
+									gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+								`,
+							}}
+						/>
+					</>
+				)}
 				<WebApplicationStructuredData />
 				<OrganizationStructuredData />
 				<WebSiteStructuredData />
